02c3249ee0b72948b354de78f1a4b1a7ab667443
Julian Ajras <julianajras@MacBook-Air.local>
2022-07-19 09:37:45 -0300

    first commit

---
+++ ejercicio.js
@@ -0,0 +1,24 @@
+  const porcentual = (prestamoBruto, interes, meses, retornoDeseado) => {
+    let balance = -1
+    let gananciaRequerida =1.1
+    if(interes <=0){
+        return 'si no hay interes no hay necesidad de calcular las ganancias'
+    }
+
+    let retornoFinal = (prestamoBruto * retornoDeseado)/100
+    while (balance < retornoFinal) {
+        gananciaRequerida += .1
+        const cuotaMensual = (prestamoBruto + (prestamoBruto/100)*interes)/meses
+    let totalActual = prestamoBruto
+    for(let i = 0; i < meses; i++){
+         totalActual = totalActual - cuotaMensual
+        let sumaIntereses = totalActual + (totalActual/100)*gananciaRequerida
+        totalActual = sumaIntereses
+        balance = sumaIntereses
+    }
+    }
+    const porcentualGananciasEsperadas = gananciaRequerida.toFixed(2)
+    return porcentualGananciasEsperadas
+  }
+      //ejemplo de como ejecutar el script
+      //console.log(porcentual(100000, 30, 12, 0))
